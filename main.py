4e5010ccf7990ecccab7981a0b1c196bacae60d3
lioil987 <lioil987085@gmail.com>
2024-03-21 23:29:52 +0330

    add header selection list and button

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 from textual.app import App, ComposeResult
 from textual.widget import Widget
 from textual.widgets import Button, Label, Header, Footer, Static, SelectionList
-from textual.containers import Container
+from textual.containers import Container, Vertical, Horizontal
 from textual.widgets.selection_list import Selection
+import json
 #-----------
+data =None
+with open('./data.json') as f:
+    temp = json.load(f)
+    data=temp;
+
 class Cloner(App):
     CSS_PATH = "./styles.tcss"
     TITLE = "Cloner"
@@ -19,10 +25,14 @@ class Cloner(App):
 
 class Expander(Widget):
     def compose(self) -> ComposeResult:
-        sel_list = SelectionList()
-        sel_list.add_option(Selection('hi',1))
-        yield Container(sel_list)
-
+        sB = SelectionList(id='cL')
+        for i in range(0,len(data['options'])):
+            sB.add_option(Selection(data['options'][i]['title'],i))
+        with Vertical():
+            yield Container(Static("hello",id="text"),id="sBox")
+            yield Container(sB,id="cBox")
+            yield Container(Button("do it"), id="bbox")
+        
 if __name__ == "__main__":
     app = Cloner()
     app.run()
