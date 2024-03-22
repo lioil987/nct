545876efbf15fa7fdf67e5fda8d3ac62935f58e3
lioil987 <lioil987085@gmail.com>
2024-03-22 13:39:36 +0330

    add do it button and config button and create a modal with inputs and add event to selection list

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #+++++++++++srource files
 from textual.app import App, ComposeResult
 from textual.widget import Widget
-from textual.widgets import Button, Label, Header, Footer, Static, SelectionList
-from textual.containers import Container, Vertical, Horizontal
+from textual.widgets import Button, Label, Header, Footer, Placeholder, Static, SelectionList, Input
+from textual.events import Mount
+from textual import on
+from textual.containers import Container, Vertical, Horizontal, Grid
 from textual.widgets.selection_list import Selection
+from textual.screen import ModalScreen
 import json
 #-----------
 data =None
+
+
 with open('./data.json') as f:
     temp = json.load(f)
     data=temp;
@@ -18,21 +23,57 @@ class Cloner(App):
 
     def compose(self,) -> ComposeResult:
         yield Header()
-        yield Expander()
+        #+++++++++++++++++++++++++
+        sB = SelectionList(id='cL')
+        self.sB = sB
+        for i in range(0,len(data['options'])):
+            sB.add_option(Selection(data['options'][i]['title'],i))
+            
+        with Vertical():
+            yield Container(Static("choose item an create theme",id="text"),id="sBox")
+            yield Container(sB,id="cBox")
+            yield Container(Button("do it",id="doit_b",disabled=True),Button('config',id="config-b"), id="bbox")
+        #-------------------------
         yield Footer()
+    def on_button_pressed(self, event: Button.Pressed) -> None | object:
+        button_id = event.button.id
+        if button_id == "config-b":
+             self.push_screen(QuitScreen())
+    @on(SelectionList.SelectedChanged)
+    def update_selected_view(self) -> None:
+        if len((self.query_one('SelectionList')).selected) == 0:
+            self.query_one("Button#doit_b").disabled = True;
+        else:
+            self.query_one("Button#doit_b").disabled = False;
+
+
+
+
+class QuitScreen(ModalScreen[bool]):  
+
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Static("Are you sure you want to quit?"),
+            Input(placeholder="write a origin file or directory or url"),
+            Input(placeholder="write a destination directory"),
+            
+            Container(Button("save", variant="default", id="save_b"),
+            id="dialog",
+        ))
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "quit":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
+
+
     
 
 
-class Expander(Widget):
-    def compose(self) -> ComposeResult:
-        sB = SelectionList(id='cL')
-        for i in range(0,len(data['options'])):
-            sB.add_option(Selection(data['options'][i]['title'],i))
-        with Vertical():
-            yield Container(Static("hello",id="text"),id="sBox")
-            yield Container(sB,id="cBox")
-            yield Container(Button("do it"), id="bbox")
-        
+
+
 if __name__ == "__main__":
     app = Cloner()
     app.run()
